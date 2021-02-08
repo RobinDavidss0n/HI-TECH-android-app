03cc4b694f304fe5bdab603359e62cb369848e73
vaad19uj <vaad19uj@student.ju.se>
2021-02-08 16:12:38 +0100

    Update MainActivity.kt

--- app/src/main/java/se/ju/student/hitech/MainActivity.kt
+++ app/src/main/java/se/ju/student/hitech/MainActivity.kt
@@ -31,14 +31,14 @@ class MainActivity : AppCompatActivity() {
 
         if (savedInstanceState == null) {
             supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragment_container, NewsFragment(), TAG_FRAGMENT_NEWS)
-                .add(R.id.fragment_container, AdminLoginFragment(), TAG_FRAGMENT_ADMIN_LOGIN)
-                .add(R.id.fragment_container, AboutFragment(), TAG_FRAGMENT_ABOUT)
-                .add(R.id.fragment_container, EventsFragment(), TAG_FRAGMENT_EVENTS)
-                .add(R.id.fragment_container, ShopFragment(), TAG_FRAGMENT_SHOP)
-                .add(R.id.fragment_container, ContactFragment(), TAG_FRAGMENT_CONTACT)
-                .commitNow()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, NewsFragment(), TAG_FRAGMENT_NEWS)
+                    .add(R.id.fragment_container, AdminLoginFragment(), TAG_FRAGMENT_ADMIN_LOGIN)
+                    .add(R.id.fragment_container, AboutFragment(), TAG_FRAGMENT_ABOUT)
+                    .add(R.id.fragment_container, EventsFragment(), TAG_FRAGMENT_EVENTS)
+                    .add(R.id.fragment_container, ShopFragment(), TAG_FRAGMENT_SHOP)
+                    .add(R.id.fragment_container, ContactFragment(), TAG_FRAGMENT_CONTACT)
+                    .commitNow()
             changeToFragment(TAG_FRAGMENT_NEWS)
         }
 
@@ -59,15 +59,6 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.options_menu, menu)
         return true
     }
-
-    /*  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-          when(item.itemId){
-              R.id.nav_login -> changeToFragment(TAG_FRAGMENT_ADMIN_LOGIN)
-              R.id.nav_about -> changeToFragment(TAG_FRAGMENT_ABOUT)
-              R.id.nav_problem -> showReportProblemAlert()
-          }
-          return true
-      }   */
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -92,17 +83,17 @@ class MainActivity : AppCompatActivity() {
 
     fun showReportProblemAlert() {
         AlertDialog.Builder(this)
-            .setTitle("Report issue")
-            .setMessage("What is the problem?")
-            .setPositiveButton(
-                "Send"
-            ) { dialog, whichButton ->
-                // Send information in textbox
-            }.setNegativeButton(
-                "Go back"
-            ) { dialog, whichButton ->
-                // Do nothing
-            }.show()
+                .setTitle("Report issue")
+                .setMessage("What is the problem?")
+                .setPositiveButton(
+                        "Send"
+                ) { dialog, whichButton ->
+                    // Send information in textbox
+                }.setNegativeButton(
+                        "Go back"
+                ) { dialog, whichButton ->
+                    // Do nothing
+                }.show()
     }
 
     fun changeToFragment(fragment_tag: String) {
@@ -118,80 +109,5 @@ class MainActivity : AppCompatActivity() {
             commit()
         }
     }
-
-    /*
-
-    val newsFragment = NewsFragment()
-    val eventsFragment = EventsFragment()
-    val hiShopFragment = ShopFragment()
-    val contactFragment = ContactFragment()
-
-
-    val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-    bottomNav.setOnNavigationItemSelectedListener {
-        when(it.itemId){
-            R.id.nav_news -> makeCurrentFragment(newsFragment)
-            R.id.nav_events -> makeCurrentFragment(eventsFragment)
-            R.id.nav_shop -> makeCurrentFragment(hiShopFragment)
-            R.id.nav_contact -> makeCurrentFragment(contactFragment)
-        }
-        true
-    }
-
-    makeCurrentFragment(newsFragment)
-
-    /*    if (savedInstanceState == null) {
-            val fragment = NewsFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.simpleName)
-                    .commit()
-        }
-
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)    */
-}
-
-private fun makeCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragment_container,fragment)
-            commit()
-        }
-
-override fun onCreateOptionsMenu(menu: Menu): Boolean {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    menuInflater.inflate(R.menu.options_menu, menu)
-    return true
-}
-
-private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
-    when (menuItem.itemId) {
-        R.id.nav_events -> {
-            val fragment = EventsFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.simpleName)
-                    .commit()
-            return@OnNavigationItemSelectedListener true
-        }
-        R.id.nav_shop -> {
-            val fragment = ShopFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.simpleName)
-                    .commit()
-            return@OnNavigationItemSelectedListener true
-        }
-        R.id.nav_news -> {
-            val fragment = NewsFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.simpleName)
-                    .commit()
-            return@OnNavigationItemSelectedListener true
-        }
-
-      /*  R.id.nav_contact -> {
-            val fragment = ContactActivity()
-            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.simpleName)
-                    .commit()
-            return@OnNavigationItemSelectedListener true
-        }   */
-    }
-    false
-}   */
-
-//}
+    
 }
