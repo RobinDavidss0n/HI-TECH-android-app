d4c415df72baa828c6a195712ebac38d309dee36
RobinDavidss0n <robind98@live.se>
2021-02-08 11:45:58 +0100

    fixed new project with empty activity

---
+++ app/src/main/java/se/ju/student/hitech/MainActivity.kt
@@ -0,0 +1,11 @@
+package se.ju.student.hitech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
