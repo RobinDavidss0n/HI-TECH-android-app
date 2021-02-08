cef2bb827a938f7ebb5f233b2f200fe741bd3f8c
vaad19uj <vaad19uj@student.ju.se>
2021-02-08 16:11:34 +0100

    logo

--- app/src/main/java/se/ju/student/hitech/MainActivity.kt
+++ app/src/main/java/se/ju/student/hitech/MainActivity.kt
@@ -25,6 +25,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setLogo(R.drawable.ic_hitech_logo)
+        supportActionBar?.setDisplayUseLogoEnabled(true)
+
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
