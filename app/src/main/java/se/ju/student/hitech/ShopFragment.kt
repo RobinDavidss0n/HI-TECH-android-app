caadf7791b2e98e81227f07a14cdd790bc42bfac
vaad19uj <vaad19uj@student.ju.se>
2021-02-11 14:45:18 +0100

    about shop

--- app/src/main/java/se/ju/student/hitech/ShopFragment.kt
+++ app/src/main/java/se/ju/student/hitech/ShopFragment.kt
@@ -1,9 +1,12 @@
 package se.ju.student.hitech
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class ShopFragment : Fragment() {
@@ -12,5 +15,18 @@ class ShopFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        view?.findViewById<Button>(R.id.button_order)?.setOnClickListener {
+            openNewTabWindow("https://forms.gle/Mh4ALSQLNcTivKtj8", this)
+        }
+    }
+
+    private fun openNewTabWindow(urls: String, context: ShopFragment) {
+        val uris = Uri.parse(urls)
+        val intents = Intent(Intent.ACTION_VIEW, uris)
+        val b = Bundle()
+        b.putBoolean("new_window", true)
+        intents.putExtras(b)
+        context.startActivity(intents)
     }
 }
