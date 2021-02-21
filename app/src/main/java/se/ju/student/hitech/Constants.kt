79a16c52f239e105a01d691505e11f273ef5cf43
vaad19uj <vaad19uj@student.ju.se>
2021-02-21 14:58:14 +0100

    Create Constants.kt

---
+++ app/src/main/java/se/ju/student/hitech/Constants.kt
@@ -0,0 +1,9 @@
+package se.ju.student.hitech
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVEY_KEY = "AAAA2zX1JqE:APA91bFDfJOI91gY_4QEWsW3kUQbMneZRFZp2SDcNnu9EKk9xCRwWyx0auDmQaUB8-NLtfVFwlJ6SZA6wBQssrKvdGyxuleKfshKdtYNkKayQ3AE1DV4JvFtW97wLT5mqAdLDAImNBry"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
