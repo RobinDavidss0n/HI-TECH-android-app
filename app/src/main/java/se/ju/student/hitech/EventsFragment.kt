6f753bdb45ca5b7b0374c495616aac736691457a
vaad19uj <vaad19uj@student.ju.se>
2021-03-08 13:04:21 +0100

    popup menu

--- app/src/main/java/se/ju/student/hitech/EventsFragment.kt
+++ app/src/main/java/se/ju/student/hitech/EventsFragment.kt
@@ -1,11 +1,13 @@
 package se.ju.student.hitech
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.MutableLiveData
@@ -52,6 +54,7 @@ class EventsFragment : Fragment() {
                     binding.rvEvents.apply {
                         layoutManager = LinearLayoutManager(context)
                         adapter = EventAdapter(it)
+
                     }
 
                     binding.fabCreateEvent.setOnClickListener {
@@ -101,6 +104,9 @@ class EventsFragment : Fragment() {
             holder.binding.tvTime.text = event.time
             holder.binding.tvTitle.text = event.title
             holder.binding.tvInformation.text = event.information
+            holder.binding.icMenu.setOnClickListener{
+              //  val popupMenu = PopupMenu(parent.context, holder.binding.icMenu)
+            }
 
         }
 
