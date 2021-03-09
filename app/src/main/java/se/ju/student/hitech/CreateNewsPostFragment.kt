b90dcb5dd84abe648d5337e0ff9138747276cad8
vaad19uj <vaad19uj@student.ju.se>
2021-03-09 15:48:52 +0100

    notification

--- app/src/main/java/se/ju/student/hitech/CreateNewsPostFragment.kt
+++ app/src/main/java/se/ju/student/hitech/CreateNewsPostFragment.kt
@@ -81,7 +81,7 @@ class CreateNewsPostFragment : Fragment() {
         createNoveltyButton.setOnClickListener {
             newsRepository.addNovelty(title.text.toString(), content.text.toString())
 
-           /* if (checked) {
+            if (checked) {
                 // send notification
                 if (title.toString() != "" && notificationContent.toString() != "") {
                     (context as MainActivity).createNotification(
@@ -92,7 +92,7 @@ class CreateNewsPostFragment : Fragment() {
                     checked = false
                 }
                 // else toast error message - fields can't be empty
-            }   */
+            }
 
 
             (context as MainActivity).changeToFragment(TAG_FRAGMENT_NEWS)
