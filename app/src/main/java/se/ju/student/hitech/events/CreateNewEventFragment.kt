99d075349f51c5495d5a0362ee153d8a623845ab
vaad19uj <vaad19uj@student.ju.se>
2021-03-17 15:30:28 +0100

    input validation create event

--- app/src/main/java/se/ju/student/hitech/events/CreateNewEventFragment.kt
+++ app/src/main/java/se/ju/student/hitech/events/CreateNewEventFragment.kt
@@ -9,6 +9,7 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputLayout
 import se.ju.student.hitech.MainActivity
 import se.ju.student.hitech.MainActivity.Companion.TAG_FRAGMENT_EVENTS
 import se.ju.student.hitech.R
@@ -37,32 +38,32 @@ class CreateNewEventFragment : Fragment() {
 
         binding.progressBar.visibility = GONE
 
-
-
         binding.btnCreateEvent.setOnClickListener {
-            binding.progressBar.visibility = VISIBLE
 
-            val title = binding.etEventActivity.text
-            val date = binding.etDate.text
-            val time = binding.etTime.text
-            val location = binding.etLocation.text
-            val information = binding.etInformation.text
+            val title = binding.etEventActivity.text.toString()
+            val date = binding.etDate.text.toString()
+            val time = binding.etTime.text.toString()
+            val location = binding.etLocation.text.toString()
+            val information = binding.etInformation.text.toString()
 
-            eventRepository.addEvent(
-                title.toString(),
-                date.toString(),
-                time.toString(),
-                location.toString(),
-                information.toString()
-            ) { result ->
-                when (result) {
-                    "successful" -> {
-                        binding.progressBar.visibility = GONE
-                        (context as MainActivity).changeToFragment(TAG_FRAGMENT_EVENTS)
-                    }
-                    "internalError" -> {
-                        binding.progressBar.visibility = GONE
-                        (context as MainActivity).makeToast(getString(R.string.failed_create_event))
+            if (verifyEventUserInputs(title, date, time, location, information)) {
+                binding.progressBar.visibility = VISIBLE
+                eventRepository.addEvent(
+                    title,
+                    date,
+                    time,
+                    location,
+                    information
+                ) { result ->
+                    when (result) {
+                        "successful" -> {
+                            binding.progressBar.visibility = GONE
+                            (context as MainActivity).changeToFragment(TAG_FRAGMENT_EVENTS)
+                        }
+                        "internalError" -> {
+                            binding.progressBar.visibility = GONE
+                            (context as MainActivity).makeToast(getString(R.string.failed_create_event))
+                        }
                     }
                 }
             }
@@ -71,5 +72,46 @@ class CreateNewEventFragment : Fragment() {
         binding.btnCreateEventBack.setOnClickListener {
             (context as MainActivity).changeToFragment(TAG_FRAGMENT_EVENTS)
         }
+    }
+
+    private fun verifyEventUserInputs(
+        title: String,
+        date: String,
+        time: String,
+        location: String,
+        information: String
+    ): Boolean {
+        binding.textInputLayoutNewEventTitle.error = ""
+        binding.textInputLayoutNewEventDate.error = ""
+        binding.textInputLayoutNewEventTime.error = ""
+        binding.textInputLayoutNewEventLocation.error = ""
+        binding.textInputLayoutNewEventInformation.error = ""
+
+        if (title.isEmpty()) {
+            binding.textInputLayoutNewEventTitle.error = getString(R.string.empty_title)
+            return false
+        }
+
+        if (date.isEmpty()) {
+            binding.textInputLayoutNewEventDate.error = getString(R.string.empty_date)
+            return false
+        }
+
+        if (time.isEmpty()) {
+            binding.textInputLayoutNewEventTime.error = getString(R.string.empty_time)
+            return false
+        }
+
+        if (location.isEmpty()) {
+            binding.textInputLayoutNewEventLocation.error = getString(R.string.empty_location)
+            return false
+        }
+
+        if (information.isEmpty()) {
+            binding.textInputLayoutNewEventInformation.error = getString(R.string.empty_information)
+            return false
+        }
+
+        return true
     }
 }
